8317c014591fe1248f2c6224a4106cffffa98e70
Aleix Boné <abone9999@gmail.com>
2025-12-06 22:47:24 +0100

    Day 6: use ZipList

--- day6.hs
+++ day6.hs
@@ -1,3 +1,4 @@
+import Control.Applicative
 import Control.Arrow
 import Data.Char
 import Data.List
@@ -6,17 +7,15 @@ import Text.ParserCombinators.ReadP
 op "*" = product
 op "+" = sum
 
-part1 s = sum $ zipWith ($) (op <$> a) (transpose $ fmap read <$> as)
+part1 s = sum . getZipList $ ZipList (op <$> a) <*> ZipList (transpose $ fmap read <$> as)
   where
     (a : as) = reverse . fmap words . lines $ s
 
-parseGroups = fst . last . readP_to_S (sepBy (endBy int eol) eol) . filter (/= ' ')
-  where
-    eol = char '\n'
-    int = read <$> munch1 isDigit :: ReadP Int
-
-part2 s = sum $ zipWith ($) (op <$> words a) (parseGroups . unlines $ reverse <$> transpose as)
+part2 s = sum . getZipList $ ZipList (op <$> words a) <*> ZipList (parseGroups . unlines $ reverse <$> transpose as)
   where
     (a : as) = reverse $ lines s
+    parseGroups = fst . last . readP_to_S (sepBy (endBy int eol) eol) . filter (/= ' ')
+    eol = char '\n'
+    int = read <$> munch1 isDigit :: ReadP Int
 
 main = getContents >>= print . (part1 &&& part2)
