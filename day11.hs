5f3f2798492bcbd95b5bdc08f37b87a46c56dd63
Aleix Boné <abone9999@gmail.com>
2025-12-11 13:00:11 +0100

    Day 11: Reactor (part 2)

--- day11.hs
+++ day11.hs
@@ -7,21 +7,31 @@ import Data.Map.Strict qualified as M
 
 parse = M.fromList . fst . last . readP_to_S (((,) <$> (munch1 isAlpha <* string ": ") <*> (munch1 isAlpha `sepBy` char ' ')) `endBy` char '\n')
 
--- convert Strings to increasing integers
-toInts :: M.Map String a -> M.Map String Int
-toInts = M.insert "out" 0 . snd . M.mapAccumWithKey (\a k _ -> (succ a, a)) 1
-
-part1 m = go you
+findPaths src dest m = go (keys M.! src)
   where
-    go 0 = 1
-    go i = sum $ (r A.!) <$> (adjList A.! i)
+    go i
+      | i == target = 1
+      | otherwise = sum $ (r A.!) <$> (adjList A.! i)
 
-    keys = toInts m
-    you = keys M.! "you"
-
-    bounds = (0, pred $ M.size keys)
     r = A.listArray bounds $ go <$> A.range bounds
 
+    target = keys M.! dest
+    bounds = (0, pred $ M.size keys)
+
+    keys = M.insert "out" 0 . snd . M.mapAccumWithKey (\a k _ -> (succ a, a)) 1 $ m
     adjList = A.listArray bounds $ [] : (fmap (keys M.!) <$> M.elems m)
 
-main = getContents >>= print . part1 . parse
+part1 = findPaths "you" "out"
+
+part2 m = svr_fft * fft_dac * dac_out + svr_dac * dac_fft * fft_out
+  where
+    svr_fft = findPaths "svr" "fft" m
+    svr_dac = findPaths "svr" "dac" m
+
+    dac_fft = findPaths "dac" "fft" m
+    fft_dac = findPaths "fft" "dac" m
+
+    fft_out = findPaths "fft" "out" m
+    dac_out = findPaths "dac" "out" m
+
+main = getContents >>= print . (part1 &&& part2) . parse
