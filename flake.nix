2fa2cfe0d02a9178079fbeb96261595bda096b6c
Aleix Boné <abone9999@gmail.com>
2025-12-11 21:49:01 +0100

    Add day packages to nix outputs

--- flake.nix
+++ flake.nix
@@ -15,7 +15,28 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        inherit (pkgs) lib;
+
         haskellPackages = pkgs.haskell.packages.ghc912;
+        buildDay =
+          name:
+          pkgs.runCommand name
+            {
+              src = ./. + ("/" + name + ".hs");
+              nativeBuildInputs = [ haskellPackages.ghc ];
+            }
+            ''
+              mkdir -p $out/bin
+              ghc -O3 $src -o $out/bin/$name
+            '';
+
+        days = lib.pipe (builtins.readDir ./.) [
+          (lib.filterAttrs (n: v: v == "regular" && lib.hasSuffix ".hs" n))
+          builtins.attrNames
+          (builtins.map (lib.removeSuffix ".hs"))
+          (lib.flip lib.genAttrs buildDay)
+        ];
+
       in
       {
         devShell = haskellPackages.shellFor {
@@ -26,6 +47,14 @@
             fourmolu
             ghcid
           ];
+        };
+
+        packages = days // {
+          all = pkgs.symlinkJoin {
+            name = "all";
+            paths = builtins.attrValues days;
+          };
+          default = self.packages.${system}.all;
         };
       }
     );
