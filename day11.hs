8d2ec95b6cf6574ef60916993a0f05bb14ea6d8d
Aleix Boné <abone9999@gmail.com>
2025-12-11 13:00:11 +0100

    Day 11: Reactor (minor optimizations)

--- day11.hs
+++ day11.hs
@@ -7,31 +7,36 @@ import Data.Map.Strict qualified as M
 
 parse = M.fromList . fst . last . readP_to_S (((,) <$> (munch1 isAlpha <* string ": ") <*> (munch1 isAlpha `sepBy` char ' ')) `endBy` char '\n')
 
-findPaths src dest m = go (keys M.! src)
+out = 0 :: Int -- index of out node
+
+buildAdjList m = (keys, adj)
+  where
+    keys = M.insert "out" out . snd . M.mapAccumWithKey (\a k _ -> (succ a, a)) (succ out) $ m
+    adj = A.listArray (out, out + M.size m) $ [] : (fmap (keys M.!) <$> M.elems m)
+
+findPaths adj src dest = go src
   where
     go i
-      | i == target = 1
-      | otherwise = sum $ (r A.!) <$> (adjList A.! i)
+      | i == dest = 1 :: Int
+      | otherwise = sum $ (r A.!) <$> (adj A.! i)
 
     r = A.listArray bounds $ go <$> A.range bounds
+    bounds = A.bounds adj
 
-    target = keys M.! dest
-    bounds = (0, pred $ M.size keys)
+part1 (keys, adj) = findPaths adj (keys M.! "you") out
 
-    keys = M.insert "out" 0 . snd . M.mapAccumWithKey (\a k _ -> (succ a, a)) 1 $ m
-    adjList = A.listArray bounds $ [] : (fmap (keys M.!) <$> M.elems m)
+-- Since graph must be acyclic, one of fft_dac or dac_fft will be 0
+part2 (keys, adj)
+  | fft_dac /= 0 = svr_fft * fft_dac * dac_out
+  | otherwise = svr_dac * dac_fft * fft_out
+    where
+      [svr, fft, dac] = (keys M.!) <$> ["svr", "fft", "dac"]
+      svr_fft = findPaths adj svr fft
+      fft_dac = findPaths adj fft dac
+      dac_out = findPaths adj dac out
 
-part1 = findPaths "you" "out"
+      svr_dac = findPaths adj svr dac
+      dac_fft = findPaths adj dac fft
+      fft_out = findPaths adj fft out
 
-part2 m = svr_fft * fft_dac * dac_out + svr_dac * dac_fft * fft_out
-  where
-    svr_fft = findPaths "svr" "fft" m
-    svr_dac = findPaths "svr" "dac" m
-
-    dac_fft = findPaths "dac" "fft" m
-    fft_dac = findPaths "fft" "dac" m
-
-    fft_out = findPaths "fft" "out" m
-    dac_out = findPaths "dac" "out" m
-
-main = getContents >>= print . (part1 &&& part2) . parse
+main = getContents >>= print . (part1 &&& part2) . buildAdjList . parse
